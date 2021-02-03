eda1a7a72b5bab675578f22fff8ef3125d84e0a3
haewon96 <eunhae96@naver.com>
2021-02-03 22:52:13 +0900

    # 기출문제 A31 : 금광

--- dynamic_programming/A-31.py
+++ dynamic_programming/A-31.py
@@ -2,12 +2,20 @@
 
 n, m = map(int, input().split())
 array = list(map(int, input().split()))
-gold_mine = []
 
+gold_mine = []
 for i in range(n):
     gold_mine.append(array[i * m : (i * m) + m])
 
-"""
+d = []
+for i in range(n):
+    d.append([0] * m)
+
+for i in range(n):
+    for j in range(m):
+        d[i][j]
+
+# 참고사항
 for i in range(n):
     for j in range(m):
         print('d[', i, '][', j, '] = ', gold_mine[i][j])
@@ -15,6 +23,5 @@ print(gold_mine)
 # 00 01 02 03
 # 10 11 12 13
 # 20 21 22 23
-"""
 
 
